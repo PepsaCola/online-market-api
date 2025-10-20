7e2c99943dbd79774ad64566f7f13b83a70a7797
PepsaCola <162103453+PepsaCola@users.noreply.github.com>
2025-10-20 11:44:02 +0200

    prepared to deploy

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ import app from "./app.js";
 import mongoose from "mongoose";
 
 dotenv.config();
-// const PORT = 3001;
+const PORT = 3001;
 // const server = http.createServer(app);
 
 const{NAME,PASS,LINK,DBNAME}=process.env;
 mongoose.connect(`mongodb+srv://${NAME}:${PASS}@${LINK}/${DBNAME}?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => {
-        // server.listen(PORT,()=>{
-        //     console.log(`Server started on port ${PORT}`);
-        // })
+        app.listen(PORT,()=>{
+            console.log(`Server started on port ${PORT}`);
+        })
         console.log('MongoDB Connected')
     })
     .catch((error) => {
         console.log(error);
         process.exit(1);
     })
-module.exports = app
+
